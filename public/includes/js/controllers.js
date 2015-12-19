fc10ca72d982bdd03b699b018e8ded75360699b9
NatalieM82 <natalim82@gmail.com>
2015-12-20 00:02:24 +0200

    Fix broken links

--- public/includes/js/controllers.js
+++ public/includes/js/controllers.js
@@ -3,8 +3,8 @@
  */
 var schoolsControllers = angular.module('schoolsControllers', []);
 
-schoolsControllers.controller('searchCtrl', ['$scope', '$http', '$timeout', '$q',
-    function ($scope, $http, $timeout, $q) {
+schoolsControllers.controller('searchCtrl', ['$scope', '$http',
+    function ($scope, $http) {
         $scope.flag=false;
 
         $http.get('http://localhost:8080/GetSchools').success(function(data) {
@@ -24,3 +24,37 @@ schoolsControllers.controller('schoolInfoCtrl', ['$scope', '$http', '$routeParam
         });
 
 }]);
+
+//
+//schoolsControllers.controller('searchCtrl', ['$scope', '$http', '$rootScope','$window',
+//    function ($scope, $http, $rootScope, $window) {
+//        $rootScope.flag=false;
+//
+//        $http.get('http://localhost:8080/GetSchools').success(function(data) {
+//            console.log(data.data);
+//            //$window.sessionStorage.schools = data.data;
+//
+//            $window.sessionStorage.setItem('schools', angular.toJson(data.data));
+//            $scope.schools = $window.sessionStorage.getItem('schools');
+//
+//            $window.sessionStorage.setItem('flag', true);
+//            //$window.sessionStorage.flag = true;
+//            $scope.flag = $window.sessionStorage.getItem('flag');
+//        });
+//
+//
+//    }]);
+//
+//schoolsControllers.controller('schoolInfoCtrl', ['$scope', '$http', '$routeParams','$rootScope','$window',
+//    function ($scope, $http, $routeParams, $rootScope, $window) {
+//        $http.get('http://localhost:8080/GetSchool?id='+ $routeParams.schoolId).success(function(data) {
+//            console.log(data.data);
+//            $scope.school = data.data;
+//            $scope.schools = $window.sessionStorage.getItem('schools');
+//            $scope.flag = $window.sessionStorage.getItem('flag');
+//        });
+//
+//
+//
+//    }]);
+
